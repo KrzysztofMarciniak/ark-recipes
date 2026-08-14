675bd6bd9407a855c8e6943c551aebd063f9468c
KrzysztofMarciniak <96542207+KrzysztofMarciniak@users.noreply.github.com>
2026-08-14 17:17:08 +0200

    redid

--- ark-hello-world/1.0/recipe.sh
+++ ark-hello-world/1.0/recipe.sh
@@ -1,12 +1,9 @@
-# recipe.sh
 ARK_SOURCE_URL="https://github.com/KrzysztofMarciniak/ark-hello-world/archive/refs/tags/1.0.tar.gz"
 ARK_SOURCE_SHA256="afb6bf287f63778a6e57274974425858f11233185c3f0e9844b79e4545d3ae39"
 ARK_TYPE="source"
 ARK_DEPENDS=""
+ARK_BINARIES="ark-hello-world"
 
 build() {
-    tar xvf "$ARK_SOURCE_ARCHIVE" -C "$ARK_BUILD_DIR"
-    cd "$ARK_BUILD_DIR"/*/
     ./build.sh
-    cp build/ark-hello-world "$HOME/.ark/bin/"
 }
